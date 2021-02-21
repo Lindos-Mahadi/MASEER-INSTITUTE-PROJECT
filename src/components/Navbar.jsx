7ab5a3091b1f4cef820a63d0088b4544c0ff4687
lindo <lindo@LINDOS>
2021-02-21 13:05:03 +0600

    menu logo setup and Design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import logo from '../components/img/logo.jpg';
+import logo from '../components/img/logo.png';
 
 const Navbar = () =>{
 
@@ -9,27 +9,24 @@ const Navbar = () =>{
         <div className="bg-dark fixed-top">
             <div className="row">
                 <div className="col-10 mx-auto">
-                <nav className="navbar navbar-expand-sm navbar-light text-dark">
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item"><a className="nav-link con-info" href=""><i className="fa fa-phone"></i> +880 1765-25120</a></li>
-                            <li className="nav-item"><a className="nav-link con-info" href=""><i className="fa fa-envelope-o"></i> info@maseerinstitute.com</a></li>
-                            <li className="nav-item"><a className="nav-link" href=""><i className="fa fa-facebook"></i></a></li>
-                            <li className="nav-item"><a className="nav-link" href=""><i className="fa fa-twitter"></i></a></li>
-                            <li className="nav-item"><a className="nav-link" href=""><i className="fa fa-instagram"></i></a></li>
-                            <li className="nav-item"><a className="nav-link" href=""><i className="fa fa-linkedin"></i></a></li>
-                            <li className="nav-item"><a className="nav-link" href=""><i className="fa fa-youtube-play"></i></a></li>
-                        </ul>
-                    </div>
-                </nav>
                 <nav className="navbar navbar-expand-lg navbar-light text-dark" id="navbar">
-                    <NavLink className="navbar-brand" to="/"><img className="img-fluid rounded-circle" src={logo} width="60" height="40" /></NavLink>
+                    <NavLink className="navbar-brand" to="/"><img className="img-fluid" src={logo} width="60" height="60" /></NavLink>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupport">
+                        
                         <ul className="navbar-nav ml-auto">
+                        <div className="d-flex justify-content-end">
+                            <p className="d-none d-md-inline-block"><a className="nav-link con-info" href=""><i className="fa fa-phone"></i> +880 1765-25120</a></p>
+                            <p className="d-none d-md-inline-block"><a className="nav-link con-info" href=""><i className="fa fa-envelope-o"></i> info@maseerinstitute.com</a></p>
+                            <p><a className="nav-link" href=""><i className="fa fa-facebook"></i></a></p>
+                            <p><a className="nav-link" href=""><i className="fa fa-twitter"></i></a></p>
+                            <p><a className="nav-link" href=""><i className="fa fa-instagram"></i></a></p>
+                            <p><a className="nav-link" href=""><i className="fa fa-linkedin"></i></a></p>
+                            <p><a className="nav-link" href=""><i className="fa fa-youtube-play"></i></a></p>
+                        </div>
                             <li className="nav-item active">
                                 <NavLink exact activeClassName="menu_active" className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
                             </li>
