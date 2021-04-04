34407310c8512eccb531eae62b34a65f030ff6f9
lindo <lindo@LINDOS>
2021-04-04 15:02:43 +0600

    Content uploaded

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,39 +16,39 @@ const NavbarMenu = (props) =>{
                 {/* <nav className="navbar navbar-expand-lg navbar-light text-dark" id="navbar">
                     <NavLink className="navbar-brand" to="/"><img className="img-fluid" src={logo} width="60" height="60" /></NavLink> */}
                     <Navbar collapseOnSelect expand="lg" text="dark">
-                    <Navbar.Brand href="/"><img className="img-fluid" src={logo} width="60" height="60" /></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                      <Navbar.Brand href="/"><img className="img-fluid" src={logo} width="60" height="60" /></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-                        <Navbar.Collapse id="responsive-navbar-nav" className="navbar">
-                            <Nav className="ml-auto nav nav-pills" >
-                                <Nav.Item className="nav-item mt-sm-5 mt-md-0">
-                                    <Nav.Link eventKey="1" activeClassName="active" className="nav-link active first-item" href="/">Home</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item >
-                                    <Nav.Link eventKey="2" activeClassName="active" className="nav-link" href="/course">Courses</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="3" activeClassName="active" className="nav-Link enrollCourse" href="/enrollments">Enrollment</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link activeClassName="active" className="nav-link" href="/notice">Notice</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link activeClassName="active" className="nav-link" href="/about">About Us</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link activeClassName="active" className="nav-link" href="contact">Contact Us</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                            <div className="d-flex justify-content-end social-list ">
-                            <p className="d-none d-sm-block"><a className="nav-link float-right" href="/contact"><i className="fa fa-phone"></i></a></p>
-                            <p className="d-none d-sm-block"><a className="nav-link float-right" href="/contact"><i className="fa fa-envelope-o"></i></a></p>
-                            <p className=""><a className="nav-link" href=""><i className="fa fa-facebook"></i></a></p>
-                            <p className="d-none d-sm-block"><a className="nav-link" href=""><i className="fa fa-twitter"></i></a></p>
-                            <p className=""><a className="nav-link" href=""><i className="fa fa-instagram"></i></a></p>
-                            <p className="d-none d-sm-block"><a className="nav-link" href=""><i className="fa fa-linkedin"></i></a></p>
-                            <p className=""><a className="nav-link" href="https://www.youtube.com/watch?v=yMbQFaiQs3A"><i className="fa fa-youtube-play"></i></a></p>
-                        </div>
+                          <Navbar.Collapse id="responsive-navbar-nav" className="navbar">
+                              <Nav className="ml-auto">
+                                  <Nav.Item className="nav-item mt-sm-5 mt-md-0">
+                                      <Nav.Link activeClassName="menu_active" className="nav-link first-item" href="/">Home</Nav.Link>
+                                  </Nav.Item>
+                                  <Nav.Item >
+                                      <Nav.Link activeClassName="menu_active" className="nav-link" href="/course">Courses</Nav.Link>
+                                  </Nav.Item>
+                                  <Nav.Item>
+                                      <Nav.Link activeClassName="menu_active" className="nav-Link enrollCourse" href="/enrollments">Enrollment</Nav.Link>
+                                  </Nav.Item>
+                                  <Nav.Item>
+                                      <Nav.Link activeClassName="menu_active" className="nav-link" href="/notice">Notice</Nav.Link>
+                                  </Nav.Item>
+                                  <Nav.Item>
+                                      <Nav.Link activeClassName="menu_active" className="nav-link" href="/about">About Us</Nav.Link>
+                                  </Nav.Item>
+                                  <Nav.Item>
+                                      <Nav.Link activeClassName="menu_active" className="nav-link" href="contact">Contact Us</Nav.Link>
+                                  </Nav.Item>
+                              </Nav>
+                              <div className="d-flex justify-content-end social-list ">
+                              <p className="d-none d-sm-block"><a className="nav-link float-right" href="/contact"><i className="fa fa-phone"></i></a></p>
+                              <p className="d-none d-sm-block"><a className="nav-link float-right" href="/contact"><i className="fa fa-envelope-o"></i></a></p>
+                              <p className=""><a className="nav-link" href=""><i className="fa fa-facebook"></i></a></p>
+                              <p className="d-none d-sm-block"><a className="nav-link" href=""><i className="fa fa-twitter"></i></a></p>
+                              <p className=""><a className="nav-link" href=""><i className="fa fa-instagram"></i></a></p>
+                              <p className="d-none d-sm-block"><a className="nav-link" href=""><i className="fa fa-linkedin"></i></a></p>
+                              <p className=""><a className="nav-link" href="https://www.youtube.com/watch?v=yMbQFaiQs3A"><i className="fa fa-youtube-play"></i></a></p>
+                          </div>
                         </Navbar.Collapse>
                     </Navbar>
             </div>
--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -13,6 +13,7 @@ const Notice=()=> {
                             <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt lorem sit amet velit tempor, a blandit neque elementum. Aenean rutrum sit amet justo quis vulputate. Morbi dapibus dolor nec lacinia tempus. Ut finibus libero sem, eget cursus risus elementum a. Phasellus congue risus in elit consequat eleifend quis nec turpis. Duis in diam mauris. Morbi pretium quis nisl vel semper. Sed eros orci, aliquam ut hendrerit eleifend, ornare et ipsum. Donec fringilla, magna sit amet pulvinar congue, purus tortor imperdiet ipsum, id pulvinar odio odio at lectus. Praesent ornare est et molestie ultricies. Nunc et tristique justo. Cras molestie sapien purus, id condimentum orci luctus ac. Donec finibus maximus felis, nec feugiat risus suscipit eget. Suspendisse tincidunt nunc vel mattis fermentum. Mauris sit amet sagittis quam. Duis ac ipsum sem. </p>
                             
                         </div>
+                        
                     </div>
                 </div>
             </section>
